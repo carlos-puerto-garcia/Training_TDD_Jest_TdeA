a10bb97aa3c822972148c65fb4574be0ec960e03
AndresLopez <aflopezm2@hotmail.com>
2017-10-28 01:12:53 -0500

    Update index.js
    Andres Felipe Lopez
    lunes - viernes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 
 
-function add() {}
+function findMax(array) {
+  var i;
+  var max = 0;
+  for(i = 0; i < array.length; i++) {
+      if (array[i] > max) {
+          max = array[i];
+      }
+  }
+  return max;
+}
 
+function square(a, b) {
+  return a * b;
+}
 
 
 
 
 module.exports = {
   
-  add,
-  
+   findMax,
+  square,
   
 };
+
