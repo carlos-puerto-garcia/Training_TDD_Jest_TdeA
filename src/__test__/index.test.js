92274e4e3d147d0884b3129076685d01e076a8ed
AndresLopez <aflopezm2@hotmail.com>
2017-10-28 01:14:04 -0500

    Update index.test.js
    Taller pruebas

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,11 +1,22 @@
 const calc = require('../index');
+const square = calc.square;
+const findMax = calc.findMax;
 
 describe('Calculator module', () => {
   
-
-  describe('add', () => {
-
+  describe('square', () => {
+		it('Deberia multipicar dos númers' , () =>{
+			expect(square(7, 3)).toBe(21);
+		
+		});
   
+  });
 
-  
+  describe('findMax', ( ) => {
+    it('debe encontrar el numero mayor del array' , () =>{
+      expect(findMax([1,6,7])).toBe(7)
+
+  });
+});
+
 });
