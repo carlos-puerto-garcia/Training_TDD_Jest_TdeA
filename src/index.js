2a3c5286901f564199bfe9dac5bc785f6beb190f
wendy1496 <33206507+wendy1496@users.noreply.github.com>
2017-10-29 19:57:28 -0500

    Wendy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 
-
 function add() {}
 
+function multiplicar(a,b){
+	return b*a;
+}
 
-
-
+function multiplos(valor) { //multiplos de dos
+	var multiplos2;
+    for(var i = 1; i < 100; i++) {
+        if (valor[i]% 2 ==0 ) {
+            multiplos2 = valor[i];
+        }
+    }
+    return multiplos2;
+  }
 
 module.exports = {
   
   add,
-  
+  multiplicar,
+  multiplos,
   
 };
